6c3d23261158268d9e5da6fc8439316c441b8b4e
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-21 22:19:10 +0530

    Add files via upload

---
+++ Iterator.py
@@ -0,0 +1,28 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+a = iter(lst)
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print("-----------------------------")
+str = "Hello World"
+b = iter(str)
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
+
+
+
